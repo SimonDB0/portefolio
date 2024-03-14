15b8bdf1eef0a79b30458a9fc15e0899a238ff4d
Simon De Boe <simondeboe7090@gmail.com>
2024-03-14 15:35:37 +0100

    commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,217 @@
+"use client";
 import Image from "next/image";
+import { useCallback, useState, useEffect } from "react";
+import gsap from "gsap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ModalCV = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+      <div className="bg-white p-4 rounded-lg max-w-xl w-full">
+        <button onClick={onClose} className="float-right font-bold">
+          X
+        </button>
+        <iframe
+          src="/cv.pdf"
+          frameBorder="0"
+          style={{ width: "100%", height: "80vh" }}></iframe>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      email: event.target.email.value,
+      message: event.target.message.value,
+    };
+
+    const response = await fetch("https://127.0.0.1:8000/api/send-email ", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      toast.success("Email envoyé avec succès !");
+    } else {
+      toast.error("Échec de l'envoi de l'email.");
+    }
+  };
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = useCallback(() => {
+    setModalOpen(true);
+  }, []);
+
+  const handleCloseModal = useCallback(() => {
+    setModalOpen(false);
+  }, []);
+
+  const scrollToSection = useCallback((sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, []);
+  useEffect(() => {
+    gsap.to(".animate-name", {
+      duration: 1,
+      y: -20,
+      opacity: 1,
+      ease: "power3.out",
+    });
+  }, []);
+
+  const technologies = [
+    { name: "React", src: "/react.svg" },
+    { name: "Symfony", src: "/symfony.svg" },
+    { name: "AdonisJS", src: "/adonis.png" },
+    { name: "WordPress", src: "/wp.webp" },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <>
+      <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="flex justify-between items-center py-4">
+            <div className="flex space-x-4">
+              <button
+                onClick={() => scrollToSection("home")}
+                className="py-2 px-3 text-gray-700 hover:text-gray-900">
+                Accueil
+              </button>
+              <button
+                onClick={() => scrollToSection("technologies")}
+                className="py-2 px-3 text-gray-700 hover:text-gray-900">
+                Technologies
+              </button>
+              <button
+                onClick={() => scrollToSection("contact")}
+                className="py-2 px-3 text-gray-700 hover:text-gray-900">
+                Contact
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div
+        id="home"
+        className="flex flex-col items-center justify-center min-h-screen bg-white pt-20">
+        <div className="w-40 h-40 mb-6">
+          <Image
+            src="/simon1.jpg"
+            alt="De Boe Simon"
+            className="rounded-full"
+            width={160}
+            height={160}
+            layout="responsive"
+          />
+        </div>
+        <div className="text-center">
+          <h1 className="text-3xl lg:text-5xl font-bold mb-2 animate-name">
+            De Boe Simon
+          </h1>
+          <p className="text-lg lg:text-2xl">Développeur Full stack</p>
+        </div>
+        <div className="flex space-x-4 justify-center items-center mt-4">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://github.com/SimonDB0"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            rel="noopener noreferrer">
+            <Image src="/github.png" alt="GitHub" width={40} height={40} />
           </a>
+          <a
+            href="https://www.linkedin.com/in/simon-de-boe/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <Image src="/linkedin.png" alt="LinkedIn" width={40} height={40} />
+          </a>
+          <button
+            onClick={handleOpenModal}
+            className="px-4 py-2 bg-slate-950 text-white rounded hover:bg-blue-700">
+            Voir mon CV
+          </button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        id="technologies"
+        className="bg-gray-200 min-h-screen flex flex-col items-center justify-center py-10 pt-20">
+        <h2 className="text-3xl font-bold mb-6">Technologies Utilisées</h2>
+        <div className="flex flex-wrap justify-center items-center gap-10">
+          {technologies.map((tech, index) => (
+            <div key={index} className="text-center">
+              <Image src={tech.src} alt={tech.name} width={80} height={80} />
+              <p className="text-xl mt-2">{tech.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div
+        id="contact"
+        className="bg-white min-h-screen flex flex-col items-center justify-center pt-20">
+        <div className="bg-white shadow-xl rounded-lg px-10 py-8 mb-10 max-w-lg w-full">
+          <h2 className="text-3xl font-bold mb-6">Contactez-moi</h2>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-gray-800 text-lg font-semibold mb-2">
+                Nom
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="form-input w-full px-3 py-2 border rounded"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-gray-800 text-lg font-semibold mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-input w-full px-3 py-2 border rounded"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="block text-gray-800 text-lg font-semibold mb-2">
+                Message
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                className="form-textarea w-full px-3 py-2 border rounded"></textarea>
+            </div>
+            <div className="text-right">
+              <button
+                className="bg-slate-950 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+                type="submit">
+                Envoyer
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </main>
+
+      <ModalCV isOpen={isModalOpen} onClose={handleCloseModal} />
+      <ToastContainer />
+    </>
   );
 }
